7e450d36d777d1810fe4ea11d5781d38e76fa1be
Mohamed Gibreel <mgibreel@lenadorsystems.com>
2024-09-04 10:28:05 +0400

    feat: add nvim confi

---
+++ .config/nvim/lua/gibreel/plugins/indent-blankline.lua
@@ -0,0 +1,10 @@
+return {
+	-- "lukas-reineke/indent-blankline.nvim",
+	-- event = { "BufReadPre", "BufNewFile" },
+	-- main = "ibl",
+	-- opts = {
+	-- 	indent = { char = "│" },
+	-- show_current_context = true,
+	-- show_current_context_start = true,
+	-- },
+}
